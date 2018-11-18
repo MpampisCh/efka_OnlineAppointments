8419fca5f6fdc2ae09f401ba3ba5447c5b8f7a57
Christina Malliou <cmmalliou@gmail.com>
2018-11-18 12:47:00 +0200

    username specific format in order to determine user role

--- src/main/resources/static/scripts/guest/authentication.js
+++ src/main/resources/static/scripts/guest/authentication.js
@@ -5,6 +5,7 @@ function login(usernameElement, passwordElement,roleElement) {
     let role = roleElement && roleElement ? roleElement : "";
     let user = { userName: username ,role: role};
 
+
     if (role=="ADMIN"){
         let prefix="A\t";
       var usernameappended=prefix.concat(username);
