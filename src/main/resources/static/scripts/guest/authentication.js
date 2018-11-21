0c4b86d867dec5d170e2fc81c92207eb69f01dc1
Christina Malliou <cmmalliou@gmail.com>
2018-11-21 20:24:58 +0200

    username specific format in order to determine user role

--- src/main/resources/static/scripts/guest/authentication.js
+++ src/main/resources/static/scripts/guest/authentication.js
@@ -66,9 +66,23 @@ $(document).ready(function() {
 
                    let user = { userName: username ,role: role};
 
-                   var fd = new FormData();
-                   fd.append( 'username', username);
-                   fd.append( 'password', password);
+                   if (role=="ADMIN"){
+                          let prefix="A\t";
+                        var usernameappended=prefix.concat(username);
+                        }
+                       else if (role=="CITIZEN"){
+                          let prefix="C\t";
+                          var usernameappended=prefix.concat(username);
+                        }
+                       else if (role=="DOCTOR"){
+                          let prefix="D\t";
+                          var usernameappended=prefix.concat(username);
+                        }
+
+                      var fd = new FormData();
+                      fd.append( 'username', usernameappended);
+                      fd.append( 'password', password);
+
 
                    $.ajax({
                       url: ROOT_PATH + '/login',
@@ -99,47 +113,6 @@ $(document).ready(function() {
                }
 
 });
-
-// function login(usernameElement, passwordElement) {
-//                   let username = usernameElement && usernameElement.value ? usernameElement.value : "";
-//                   let password = passwordElement && passwordElement.value ? passwordElement.value : "";
-//                   let e = document.getElementById("roleselection");
-//                   let role = e.options[e.selectedIndex].value;
-//
-//
-//                   let user = { userName: username ,role: role};
-//
-//                   var fd = new FormData();
-//                   fd.append( 'username', username);
-//                   fd.append( 'password', password);
-//
-//                   $.ajax({
-//                      url: ROOT_PATH + '/login',
-//                      data: fd,
-//                      processData: false,
-//                      contentType: false,
-//                      type: 'POST',
-//                      success: function(data){
-//                      sessionStorage.setItem(SESSION_STORAGE_LOGIN_TOKEN_NAME, JSON.stringify(user));
-//                      let json = JSON.parse(sessionStorage.getItem(SESSION_STORAGE_LOGIN_TOKEN_NAME));
-//                        if (json.role=="ADMIN"){
-//                            window.location.replace(ROOT_PATH + "/pages/admin/index.html");
-//                        }
-//                        else if (json.role=="CITIZEN"){
-//                            window.location.replace(ROOT_PATH + "/pages/citizen/index.html");
-//                        }
-//                        else if (json.role=="DOCTOR"){
-//                            window.location.replace(ROOT_PATH + "/pages/doctor/index.html");
-//                        }
-//                      },
-//                      statusCode: {
-//                        401 : function() {
-//                                alert("Invalid username or password!");
-//                            }
-//                        }
-//                    });
-//
-//               }
 
 
 
