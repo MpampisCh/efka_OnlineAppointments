3a06d647bb1a018eb444d7dced8c3b85a940cbbe
ktsak <kdtsakiri@gmail.com>
2018-11-22 13:04:35 +0200

    Update guest-auth.js

--- src/main/resources/static/scripts/security/guest-auth.js
+++ src/main/resources/static/scripts/security/guest-auth.js
@@ -6,7 +6,7 @@ if (sessionStorage.getItem(SESSION_STORAGE_LOGIN_TOKEN_NAME)) {
     else if (json.role == "CITIZEN") {
         window.location.replace(ROOT_PATH + "/pages/citizen/index.html");
     }
-    else if (json.role == DOCTOR") {
+    else if (json.role == "DOCTOR") {
         window.location.replace(ROOT_PATH + "/pages/doctor/index.html");
     }
 }
