a1bbe857b056082f4c595a48cab1815b8d10e2cf
Christina Malliou <cmmalliou@gmail.com>
2018-11-30 21:40:37 +0200

    Update login page

--- src/main/resources/static/scripts/security/guest-auth.js
+++ src/main/resources/static/scripts/security/guest-auth.js
@@ -1,9 +1,6 @@
 if (sessionStorage.getItem(SESSION_STORAGE_LOGIN_TOKEN_NAME)) {
     let json = JSON.parse(sessionStorage.getItem(SESSION_STORAGE_LOGIN_TOKEN_NAME));
-    if (json.role == "ADMIN") {
-        window.location.replace(ROOT_PATH + "/pages/admin/index.html");
-    }
-    else if (json.role == "CITIZEN") {
+    if (json.role == "CITIZEN") {
         window.location.replace(ROOT_PATH + "/pages/citizen/index.html");
     }
     else if (json.role == "DOCTOR") {
